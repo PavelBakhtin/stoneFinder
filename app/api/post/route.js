0f31032089684078351797128d232889386091b7
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-06-30 16:02:44 +0300

    14

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -5,23 +5,13 @@ export const GET = async (request) => {
       await connectToDb();
       const posts = await Post.find({}).sort({ _id: -1 }).populate('creator');
 
-      // return new Response(JSON.stringify(posts), { status: 200 });
-      const response = new Response(JSON.stringify(posts), {
-         status: 200
+      return new Response(JSON.stringify(posts), {
+         status: 200,
+         headers: {
+            referer: referer,
+            'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate'
+         }
       });
-
-      // Add a unique identifier to the URL to force a cache-busting reload
-      const url = new URL(request.url);
-      url.searchParams.set('t', Date.now());
-      response.headers.set(
-         'Cache-Control',
-         'no-cache, no-store, must-revalidate'
-      );
-      response.headers.set('Pragma', 'no-cache');
-      response.headers.set('Expires', '0');
-      response.headers.set('Location', url.toString());
-
-      return response;
    } catch (error) {
       return new Response(
          JSON.stringify('Failed top fetch all posts', { status: 500 })
