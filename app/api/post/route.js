391892aa6b49d0656d1bd12a30d6d6b44b00c688
PavelBakhtin <pavelbakhtin88@gmail.com>
2023-06-30 16:09:23 +0300

    14

--- app/api/post/route.js
+++ app/api/post/route.js
@@ -1,10 +1,13 @@
 import { connectToDb } from '@utils/database';
 import Post from '@models/post';
+import { headers } from 'next/headers';
 export const GET = async (request) => {
+   const headersList = headers();
+   const referer = headersList.get('referer');
    try {
       await connectToDb();
-      const posts = await Post.find({}).sort({ _id: -1 }).populate('creator');
 
+      const posts = await Post.find({}).populate('creator');
       return new Response(JSON.stringify(posts), {
          status: 200,
          headers: {
